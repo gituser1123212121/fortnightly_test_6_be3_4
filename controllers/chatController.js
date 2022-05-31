d8997cf16f2a0ae57e8ef2fb07c52c90c935c25c
Kshitij Kotasthane <kshitij.kotasthane@gmail.com>
2022-05-31 21:14:02 +0530

    added comments to the controllers

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -3,7 +3,7 @@ const Message = require("../models").message;
 const { Op } = require("sequelize");
 const Group = require("../models").group;
 
-// create a new message object, that represents a message from one \
+// create a new message object, that represents a message from one
 // user to the other
 const sendMessageToSingleUser = async (req, res, next) => {
   // the receiver
@@ -14,7 +14,7 @@ const sendMessageToSingleUser = async (req, res, next) => {
     userId: req.body.userId,
     senderUserId: req.body.senderUserId,
     message: req.body.message,
-    isGroupMessage: req.body.isGroupMessage,
+    isGroupMessage: false,
     sentOn: Date.now(),
   };
   // verify if both users are valid
@@ -46,7 +46,6 @@ const sendMessageToSingleUser = async (req, res, next) => {
     receiver = _receiver;
   });
 
-  // needs
   if (sender && receiver) {
     // users are valid
     // message can be sent
@@ -61,7 +60,9 @@ const sendMessageToSingleUser = async (req, res, next) => {
         });
       });
   } else {
-    res.status(404).json({ message: "user not exists" });
+    res
+      .status(404)
+      .json({ message: "either receiver or sender does not exist" });
   }
 };
 
@@ -69,6 +70,7 @@ const sendMessageToSingleUser = async (req, res, next) => {
 const getMessagesFromSpecificUser = async (req, res, next) => {
   const userId = req.params.userId;
   const senderUserId = req.params.senderUserId;
+  // wait for this operation to complete
   await Message.findAll({
     where: {
       userId: { [Op.eq]: userId },
@@ -77,6 +79,7 @@ const getMessagesFromSpecificUser = async (req, res, next) => {
     },
   })
     .then((msgs) => {
+      // send only the senderUserId and actual message body
       let formattedMessages = [];
       msgs.forEach((msg, idx) => {
         formattedMessages.push({
@@ -98,18 +101,23 @@ const getMessagesFromSpecificUser = async (req, res, next) => {
 // create a new message object that represents a group message
 const sendMessageToGroup = async (req, res, next) => {
   // create Message and GroupMessage instance
+  //  "userId": 1,
+  // "senderUserId": 2,
+  // user 2 is sending to group 1
   const groupChatObj = {
     userId: req.body.userId,
     senderUserId: req.body.senderUserId,
-    isGroupMessage: req.body.isGroupMessage,
+    isGroupMessage: true,
     message: req.body.message,
     sentOn: Date.now(),
   };
 
+  // check to make sure group exists
   await Group.findOne({
     where: { groupId: { [Op.eq]: groupChatObj.userId } },
   }).then((group) => {
     if (group) {
+      // if group exists then create a new message object
       Message.create(groupChatObj)
         .then((groupMsg) => {
           res
@@ -132,23 +140,38 @@ const getMessagesFromGroup = async (req, res, next) => {
   const userId = req.params.userId;
   const groupId = req.params.groupId;
 
-  await Message.findAll({
-    where: {
-      userId: { [Op.eq]: groupId },
-      isGroupMessage: { [Op.eq]: true },
-    },
-  }).then((groupMsgs) => {
-    let formattedGroupMessages = [];
-    groupMsgs.forEach((msg, idx) => {
-      formattedGroupMessages.push({
-        groupId: groupId,
-        messageBody: msg.message,
+  // find all messages from the Message model where isGroupMessage is true
+  // check to see if user is part of the group
+  Group.findOne({
+    where: { userId: { [Op.eq]: userId }, groupId: { [Op.eq]: groupId } },
+  }).then((userGroupFound) => {
+    if (userGroupFound) {
+      // if user is a part of specific froup, send the message
+      Message.findAll({
+        where: {
+          userId: { [Op.eq]: groupId },
+          isGroupMessage: { [Op.eq]: true },
+        },
+      }).then((groupMsgs) => {
+        // format the messages to send only groupId, and actual message
+        let formattedGroupMessages = [];
+        groupMsgs.forEach((msg, idx) => {
+          formattedGroupMessages.push({
+            groupId: groupId,
+            messageBody: msg.message,
+          });
+        });
+        // send the group messages as JSON
+        res.status(200).send({
+          message: `group messages fetched successfully`,
+          grpMessages: formattedGroupMessages,
+        });
       });
-    });
-    res.status(200).send({
-      message: `group messages fetched successfully`,
-      grpMessages: formattedGroupMessages,
-    });
+    } else {
+      res
+        .status(404)
+        .json({ message: `user is not a part of specified group` });
+    }
   });
 };
 
--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,17 +1,68 @@
 const Group = require("../models").group;
+const User = require("../models").user;
+const { Op } = require("sequelize");
 
 // create a new group row
 const createGroup = async (req, res, next) => {
   try {
+    // check for users (valid users) can be added
     let usersList = req.body.users;
     let groupId = req.body.groupId;
-    usersList.forEach((user, idx) => {
-      Group.create({ userId: user.userId, groupId: groupId });
+    // for each user do the following
+    /**
+     * for(let i=0; i<usersList.length; i++){
+     *   Group.findOne({
+        where: {
+          userId: { [Op.eq]: usersList[i].userId },
+          groupId: { [Op.eq]: groupId },
+        },
+      }).
+     * 
+     * }
+     * 
+     * findOne -> returns an object
+     * 1 A 
+     * 1 B
+     * 1 C
+     * 
+     * findAll -> returns an array
+     * 
+     * 
+     */
+    usersList.forEach((user) => {
+      // this loop will ruin for each user
+      // verifying that the user-group combination does not exist
+      Group.findOne({
+        where: {
+          userId: { [Op.eq]: user.userId },
+          groupId: { [Op.eq]: groupId },
+        },
+      }).then((groupFound) => {
+        // if the row exists, the groupFound will no tbe null
+        // else it will be null
+        // groupFound = null
+        // !groupFound == !null/!false == true
+        if (!groupFound) {
+          // add user to group if they are not a part of it
+          // or group dosen't exist
+          Group.create({ userId: user.userId, groupId: groupId });
+          // if you send response here, it will create a problem
+          // you will send a response mutiple times
+          // user1 -> send
+          // user2 -> send
+        }
+        // if the row already exists it will be ignored
+        // groupFound = valid object = {}
+        // !groupFound == !true == false
+        // if(false) -> inner code does not execute
+        // if(true) -> code will execute
+      });
     });
 
+    // send success message
     res.status(200).json({ message: `group created successfully` });
   } catch (err) {
-    res.status(500).json({ message: `internal server error: ${err.message}` });
+    res.status(500).json({ message: `internal server error: ${err}` });
   }
 };
 
